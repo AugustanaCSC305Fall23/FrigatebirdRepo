eb4c43444c1e560364603e4c3a247f682c6937c3
aaron <aaronyo2002@gmail.com>
2023-10-10 11:44:13 -0500

    Adding seach functionallity attempt1

--- Gymnastics Planner/src/main/java/edu/augustana/PrimaryController.java
+++ Gymnastics Planner/src/main/java/edu/augustana/PrimaryController.java
@@ -2,12 +2,13 @@ package edu.augustana;
 
 import java.io.*;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.TilePane;
@@ -15,15 +16,30 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
-public class PrimaryController {
+public class PrimaryController{
 
     private String dataCsvPath = "DEMO1Pack/DEMO1.csv";
     private ArrayList<Card> allCards;
+
+    @FXML
+    private TextField searchedWord;
+
+    @FXML
+    Button searchButton;
+
     @FXML
     private TilePane allCardContent;
 
     private HashMap<String, HashMap> cardsDictionary;
 
+    @FXML
+    void searchButtonAction() {
+        searchButton.setOnAction(event -> {
+            String searchText = searchedWord.getText();
+            // Do something with the collected text (e.g., perform a search)
+            searchList(searchText, allCards);
+        });
+    }
 
     void buildCards() throws IOException {
 
@@ -37,15 +53,15 @@ public class PrimaryController {
         String line = reader.readLine();
 
         //creates new cards for all csv files data
-        while(line != null){
+        while (line != null) {
             String[] splittedLine = line.split(",");
             Card newCard = new Card(splittedLine);
             allCards.add(newCard);
 
             //map Created for searching
-            HashMap<String,Card> genderMap = new HashMap<>();
+            HashMap<String, Card> genderMap = new HashMap<>();
             genderMap.put(newCard.getGender(), newCard);
-            cardsDictionary.put(newCard.getEvent(),genderMap);
+            cardsDictionary.put(newCard.getEvent(), genderMap);
 
 
             line = reader.readLine();
@@ -60,19 +76,78 @@ public class PrimaryController {
             String event = entry.getKey();
             Map<String, Card> genderMap = entry.getValue();
 
+            System.out.println("Event: " + event);
+
             for (Map.Entry<String, Card> genderEntry : genderMap.entrySet()) {
                 String gender = genderEntry.getKey();
                 Card card = genderEntry.getValue();
 
+                System.out.println("  Gender: " + gender);
+                System.out.println("  Card Details: " + card.toString());
             }
         }
     }
 
 
     private void dynamicCarAddingToView() {
-        allCardContent.setPrefColumns(3);  // Set the desired number of columns
+        for (Card card : allCards) {
+            Button button = new Button();
+
+            // Set the text and style for the button
+            String imageLink = card.getImage();
+
+            try {
+                // Load the image and create an ImageView
+
+                Image img = new Image(imageLink);
+                ImageView imgView = new ImageView(img);
+                imgView.setFitHeight(200);
+                imgView.setFitWidth(200);
+                imgView.setPreserveRatio(true);
+                Text event = new Text(card.getEvent());
+                VBox cardContentBox = new VBox(imgView, event);
+                button.setGraphic(cardContentBox);
+                allCardContent.getChildren().add(button);
+
+
+                button.setGraphic(imgView);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            // Add the button to the TilePane
+        }
+
+        // Set gaps for the TilePane
+        allCardContent.setHgap(10);
+        allCardContent.setVgap(20);
+    }
+
+    private void searchList(String inputWord, ArrayList<Card> allCards) {
+        List<String> searchWordArray = Arrays.asList(inputWord.trim().split(" "));
+        ArrayList<Card> filteredCards = new ArrayList<>();
 
         for (Card card : allCards) {
+            boolean containsAllWords = true;
+            for (String word : searchWordArray) {
+                if (!card.toString().toLowerCase().contains(word.toLowerCase())) {
+                    containsAllWords = false;
+                    break; // No need to continue checking if one word is not contained
+                }
+            }
+            if (containsAllWords) {
+                filteredCards.add(card);
+            }
+        }
+        System.out.println(filteredCards);
+        allCardContent.getChildren().clear();
+        dynamicCarAddingToViewProto(filteredCards);
+    }
+    private void dynamicCarAddingToViewProto(ArrayList<Card> filteredCards) {
+        allCardContent.setPrefColumns(3);
+
+        for (Card card : filteredCards) {
             Button button = new Button();
 
             // Set the text and style for the button
@@ -85,25 +160,22 @@ public class PrimaryController {
                 ImageView imgView = new ImageView(img);
                 imgView.setFitHeight(250);
                 imgView.setFitWidth(250);
-
-                Text event  = new Text(card.getEvent());
+                Text event = new Text(card.getEvent());
                 event.setFont(Font.font(20));
-                VBox cardContentBox = new VBox(imgView , event );
+                VBox cardContentBox = new VBox(imgView, event);
+                button.setGraphic(cardContentBox);
+                allCardContent.getChildren().add(cardContentBox);
 
-                button.setGraphic(cardContentBox);
-                allCardContent.getChildren().add(button);
-                button.setGraphic(cardContentBox);
+
+                button.setGraphic(imgView);
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
-            // Add the button to the TilePane
         }
 
         // Set gaps for the TilePane
         allCardContent.setHgap(10);
         allCardContent.setVgap(20);
-
     }
-
 }
+
