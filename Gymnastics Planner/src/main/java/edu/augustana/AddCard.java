a957ffa566324882567b109f7a9257adbf284f72
aaron <aaronyo2002@gmail.com>
2023-11-07 17:50:06 -0600

    Add Card with some issues with data entry

--- Gymnastics Planner/src/main/java/edu/augustana/AddCard.java
+++ Gymnastics Planner/src/main/java/edu/augustana/AddCard.java
@@ -49,7 +49,7 @@ public class AddCard {
                     Files.copy(sourcePath, destinationPath);
                     System.out.println("File copied successfully to: " + destinationPath);
 
-                    String newFileName = allCards.size() + 1 + ".png"; // Specify the new file name
+                    String newFileName = allCards.size() + 2 + ".png"; // Specify the new file name
                     Path newFilePath = Paths.get("C:\\git\\FrigatebirdRepo\\DEMO1Pack\\Images", newFileName);
                     Files.move(destinationPath, newFilePath);
                     System.out.println("File renamed successfully to: " + newFilePath);
@@ -63,8 +63,8 @@ public class AddCard {
 
     public void addCard(String ID, String Event, String Category, String Name, String Equipment, String Level,
                         String Gender, String Sex, String Keyword, String packFolder) {
-        String csvLine = String.format("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t\"%s\"",
-                ID, Event, Category, Name, packFolder, (allCards.size() + 1) + ".png", Gender, Sex, Level, Equipment, Keyword);
+        String csvLine = String.format("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,\"%s\"",
+                ID, Event, Category, Name, "Demo1", (allCards.size() + 1) + ".png", Gender, Sex, Level, Keyword, Equipment, packFolder);
 
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(dataCsvPath, true))) {
