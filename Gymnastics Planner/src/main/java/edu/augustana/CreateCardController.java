d43b628a4fc6b8787aaab28705e5faa64b2df80d
aaron <aaronyo2002@gmail.com>
2023-12-07 14:36:42 -0600

    Fixes 2

--- Gymnastics Planner/src/main/java/edu/augustana/CreateCardController.java
+++ Gymnastics Planner/src/main/java/edu/augustana/CreateCardController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -12,9 +13,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 
 public class CreateCardController {
@@ -58,6 +58,12 @@ public class CreateCardController {
     @FXML
     Button done_Button;
 
+    @FXML
+    Button addCardButton;
+
+
+
+
     AddCard addCard = new AddCard(imageName, dataCsvPath);
 
     @FXML
@@ -97,6 +103,93 @@ public class CreateCardController {
         keyword.clear();
         packFolder.clear();
         imageName.clear();
+    }
+
+
+    @FXML
+    private void addCardButtonAction() {
+
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Select a Directory");
+
+        // Show the directory chooser dialog
+        Stage stage = (Stage) addCardButton.getScene().getWindow(); // Replace yourButton with the actual reference to your button
+        File selectedDirectory = chooser.showDialog(stage);
+
+        boolean imagesDirFound = false;
+        boolean thumbsDirFound = false;
+        boolean csvFileFound = false;
+
+        File[] allFiles = selectedDirectory.listFiles();
+
+        int count = 0;
+
+        // Iterate through allFiles
+        for (File file : allFiles) {
+            if (count < 3) {
+                if (file.isDirectory() && file.getName().equals("Images")) {
+                    imagesDirFound = true;
+                } else if (file.isDirectory() && file.getName().equals("thumbs")) {
+                    thumbsDirFound = true;
+                } else if (file.isFile() && file.getName().endsWith(".csv")) {
+                    csvFileFound = true;
+                }
+            } else {
+                break;
+            }
+        }
+
+        String destinationPath = "AllPacks/"+selectedDirectory.getName();
+        String fileFromWhere = selectedDirectory.getAbsolutePath();
+
+        if (imagesDirFound && thumbsDirFound && csvFileFound && count == 2) {
+            Path sourcePath = Paths.get(fileFromWhere);
+            Path targetPath = Paths.get(destinationPath);
+
+            try {
+                copyDirectory(sourcePath, targetPath);
+                System.out.println("Directory copied successfully!");
+            } catch (IOException e) {
+                System.err.println("Error copying directory: " + e.getMessage());
+            }
+
+        }else{
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setContentText("Directory type not matched.");
+            alert.show();
+        }
+    }
+
+    private static void copyDirectory(Path source, Path destination) throws IOException {
+        Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                Path targetDir = destination.resolve(source.relativize(dir));
+                Files.createDirectories(targetDir);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Path targetFile = destination.resolve(source.relativize(file));
+                Files.copy(file, targetFile, StandardCopyOption.REPLACE_EXISTING);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                // Handle file visit failure if necessary
+                System.err.println("Failed to copy file: " + file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                // Handle directory visit completion if necessary
+                return FileVisitResult.CONTINUE;
+            }
+        });
     }
 
 
